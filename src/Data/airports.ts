ac1b691a04152a3889cd57eac6bbbc62aad1223f
Christopher Weilacker <39093620+chrisweilacker@users.noreply.github.com>
2021-06-01 21:47:01 -0700

    Update airports.ts

--- src/Data/airports.ts
+++ src/Data/airports.ts
@@ -7,6 +7,24 @@ export default [
       latitude: 32.8998,
       longitude: 97.0403,
     },
+  }, 
+  {
+    code: 'IAH',
+    city: 'Houston',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 29.9902,
+      longitude: 95.3368,
+    },
+  },
+  {
+    code: 'SAT',
+    city: 'San Antonio',
+    timezone: 'America/Chicago',
+    location: {
+      latitude: 29.4241,
+      longitude: 98.4936,
+    },
   },
   {
     code: 'JFK',
@@ -15,6 +33,15 @@ export default [
     location: {
       latitude: 40.6413,
       longitude: 73.7781,
+    },
+  },
+  {
+    code: 'PHX',
+    city: 'Phoenix',
+    timezone: 'America/Phoenix',
+    location: {
+      latitude: 33.4484,
+      longitude: 112.0740,
     },
   },
   {
@@ -27,6 +54,24 @@ export default [
     },
   },
   {
+    code: 'SAN',
+    city: 'San Diego',
+    timezone: 'America/Los_Angeles',
+    location: {
+      latitude: 32.7157,
+      longitude: 117.1611,
+    },
+  },
+  {
+    code: 'SJC',
+    city: 'San Jose',
+    timezone: 'America/Los_Angeles',
+    location: {
+      latitude: 37.3382,
+      longitude: 121.8863,
+    },
+  },
+  {
     code: 'ORD',
     city: 'Chicago',
     timezone: 'America/Chicago',
@@ -34,5 +79,16 @@ export default [
       latitude: 41.9742,
       longitude: 87.9073,
     },
+    
+  },
+  {
+    code: 'PHL',
+    city: 'Philadelphia',
+    timezone: 'America/New_York',
+    location: {
+      latitude: 39.9526,
+      longitude: 75.1652,
+    },
+    
   },
 ] as Airport[];
